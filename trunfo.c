65fa108c4ea1fdea53abc30c4a20a9106e6772bb
NYVELECK99 <202501635903@alunos.estacio.br>
2025-02-17 12:07:31 -0300

    Update trunfo.c
    base

--- trunfo.c
+++ trunfo.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+int populacao;
+int pontosturisticos;
+float area;
+float pib;
+char cidade[40];
+
+
+printf("%d", populacao)
+printf("%d", pontosturisticos)
+printf("%f", area)
+printf("%f", pib)
+printf("%s", cidade);
+
+}
